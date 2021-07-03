1bfef268660831f76985b6dd9360b315560a1fc6
SOUBHAGYA32 <soubhagya801832@gmail.com>
2021-07-03 20:06:46 +0530

    Hello World React

---
+++ src/User.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function User() {
+  return (
+    <div className="App">
+     <h1>Hello User</h1>
+    </div>
+  );
+}
+
+export default User;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import User from './User';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <User />
   </React.StrictMode>,
   document.getElementById('root')
 );
